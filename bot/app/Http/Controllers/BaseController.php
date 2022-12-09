841bf6ce08861558861a9c8aa2f411272e0b8b1b
Ahmad Rahimie <newrehmi@gmail.com>
2022-12-09 17:45:04 +0800

    Update BaseController.php

--- bot/app/Http/Controllers/BaseController.php
+++ bot/app/Http/Controllers/BaseController.php
@@ -1,13 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller;
+use Sigil\Controller;
 
 class BaseController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+//    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
